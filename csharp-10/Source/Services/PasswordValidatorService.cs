8638aeeccfa7a4d97b53840dcd13e836c50c75d5
dan-primo <danielly.primo@gmail.com>
2020-07-18 15:26:23 -0300

    novos testes apos code review

--- csharp-10/Source/Services/PasswordValidatorService.cs
+++ csharp-10/Source/Services/PasswordValidatorService.cs
@@ -16,7 +16,10 @@ namespace Codenation.Challenge.Services
 
         public Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
         {
-            User userFind = _dbContext.Users.FirstOrDefault(user => user.Email == context.UserName && user.Password == context.Password);
+            //_dbContext.Users.Where(x => x.Email == email).FirstOrDefault();
+            //User userFind = _dbContext.Users.FirstOrDefault(user => user.Email == context.UserName && user.Password == context.Password);
+
+            User userFind = _dbContext.Users.Where(x => x.Email == context.UserName).FirstOrDefault();
 
             if (userFind != null)
                 context.Result = new GrantValidationResult(subject: userFind.Id.ToString(), authenticationMethod: "custom", claims: UserProfileService.GetUserClaims(userFind));
