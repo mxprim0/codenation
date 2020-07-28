923e70d2bbcbad9ae9225f9a04213ccfbfb0d182
dan-primo <danielly.primo@gmail.com>
2020-07-28 14:02:34 -0300

    restauracao db feita com sucesso

--- csharp-10/Source/Models/CodenationContext.cs
+++ csharp-10/Source/Models/CodenationContext.cs
@@ -20,7 +20,7 @@ namespace Codenation.Challenge.Models
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
-                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Codenation;Trusted_Connection=True");
+                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Codenation;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
