41c6be01509b188642299728ce06daba61789639
dan-primo <danielly.primo@gmail.com>
2020-07-28 14:07:46 -0300

    update Source/Services/PasswordValidatorService.cs

--- csharp-10/Source/Services/PasswordValidatorService.cs
+++ csharp-10/Source/Services/PasswordValidatorService.cs
@@ -3,21 +3,31 @@ using IdentityServer4.Models;
 using IdentityServer4.Validation;
 using System.Linq;
 using System.Threading.Tasks;
- 
+
 namespace Codenation.Challenge.Services
 {
-    public class PasswordValidatorService: IResourceOwnerPasswordValidator
+    public class PasswordValidatorService : IResourceOwnerPasswordValidator
     {
+        private readonly CodenationContext _dbContext;
         public PasswordValidatorService(CodenationContext dbContext)
-        {            
+        {
+            _dbContext = dbContext;
         }
 
         public Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
-        {          
-            context.Result = new GrantValidationResult(
-                TokenRequestErrors.InvalidGrant, "Invalid username or password");
+        {
+            //_dbContext.Users.Where(x => x.Email == email).FirstOrDefault();
+            //User userFind = _dbContext.Users.FirstOrDefault(user => user.Email == context.UserName && user.Password == context.Password);
+
+            User userFind = _dbContext.Users.Where(x => x.Email == context.UserName).FirstOrDefault();
+
+            if (userFind != null && userFind.Password == context.Password)
+                //if (userFind != null)
+                context.Result = new GrantValidationResult(subject: userFind.Id.ToString(), authenticationMethod: "custom", claims: UserProfileService.GetUserClaims(userFind));
+            else
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant, "Invalid username or password");
             return Task.CompletedTask;
         }
-     
+
     }
 }
